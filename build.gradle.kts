9e5d0244d670d9076cfaec03ef2a02d73be00c24
victor.petukhov <victor.petukhov@jetbrains.com>
2019-02-26 17:05:14 +0300

    Remove redundant gradle tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.net.URL
-import java.nio.charset.StandardCharsets
 
 plugins {
     kotlin("jvm") version "1.3.11"
-    antlr
     `maven-publish`
 }
 
 group = "org.jetbrains.kotlin"
 version = "0.1"
+
+val archivePrefix = "kotlin-grammar-tools"
+val jar: Jar by tasks
+jar.archiveName = "$archivePrefix.jar"
 
 repositories {
     mavenCentral()
@@ -17,7 +18,6 @@ repositories {
 }
 
 dependencies {
-    antlr("org.antlr:antlr4:4.7.1")
     compile(kotlin("stdlib-jdk8"))
     compile("org.jetbrains.kotlin.spec.grammar:kotlin-grammar-parser:0.1")
 }
@@ -26,7 +26,7 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = group as String
-            artifactId = "kotlin-grammar-tools"
+            artifactId = archivePrefix
             version = version as String
 
             from(components["java"])
@@ -36,23 +36,4 @@ publishing {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.withType<AntlrTask> {
-    outputDirectory = File("src/main/java/org/jetbrains/kotlin/grammar/parser").also { it.mkdirs() }
-    arguments.add("-package")
-    arguments.add("org.jetbrains.kotlin.grammar.parser")
-}
-
-tasks.create("updateGrammar") {
-    doFirst {
-        val grammarUrlPrefix = "https://raw.githubusercontent.com/JetBrains/kotlin-spec/master/src/grammar/"
-        val grammarLocalPathPrefix = "src/main/antlr/".also { File(it).mkdirs() }
-
-        setOf("UnicodeClasses.g4", "KotlinLexer.g4", "KotlinParser.g4").forEach {
-            File(grammarLocalPathPrefix + it).writeText(
-                String(URL(grammarUrlPrefix + it).openStream().readBytes(), StandardCharsets.UTF_8)
-            )
-        }
-    }
 }
